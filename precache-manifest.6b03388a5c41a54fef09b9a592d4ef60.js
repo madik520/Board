02b1d446df465991749c1834ee8c5d8a409ccab0
Ілля С. Феденко <i.fedenko@PROBATION.LOCAL>
2020-02-04 17:41:55 +0200

    production version

---
+++ precache-manifest.6b03388a5c41a54fef09b9a592d4ef60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f224c0c843ec15b2726d8987b5cb92",
+    "url": "/Board/index.html"
+  },
+  {
+    "revision": "ef972ba27853c8f01838",
+    "url": "/Board/static/css/main.01c369f7.chunk.css"
+  },
+  {
+    "revision": "52ca20e39715330bb491",
+    "url": "/Board/static/js/2.fa3a6e34.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Board/static/js/2.fa3a6e34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef972ba27853c8f01838",
+    "url": "/Board/static/js/main.fb9e93b5.chunk.js"
+  },
+  {
+    "revision": "669704f460a5c438665f",
+    "url": "/Board/static/js/runtime-main.0c51ef9c.js"
+  },
+  {
+    "revision": "e95db44fce6558a0ed42278f20d1b5d9",
+    "url": "/Board/static/media/closeIcon.e95db44f.svg"
+  },
+  {
+    "revision": "3c0d526e6e058a0a65a99117bcc672a0",
+    "url": "/Board/static/media/logo.3c0d526e.svg"
+  }
+]);
